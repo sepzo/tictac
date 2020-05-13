73b6d5625321af738885fea29394bafa253fcf72
Demo account SSH <sushil342@gmail.com>
2020-05-13 18:39:29 +0530

    Add files via upload

---
+++ Assets/Scenes/Scripts/Board.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{   
+
+    public GameObject mCellPrefab;
+    private Cell[] mCells = new Cell[9];
+
+    void Start()
+    {
+        Build(); 
+    }
+
+    public void Build()
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            GameObject newCell = Instantiate(mCellPrefab, transform);
+            mCells[i] = newCell.GetComponent<Cell>();
+        }
+
+    }
+}
